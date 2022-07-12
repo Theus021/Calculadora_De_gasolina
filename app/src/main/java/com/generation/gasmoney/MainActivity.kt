5136e5f961e16ed30f1fdf772bfea513ac2c496a
Theus021 <mateusoliver2109@hotmail.com>
2022-07-12 19:50:52 -0300

    Parte lógica I

--- app/src/main/java/com/generation/gasmoney/MainActivity.kt
+++ app/src/main/java/com/generation/gasmoney/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.generation.gasmoney
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.generation.gasmoney.databinding.ActivityMainBinding
@@ -10,11 +9,40 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var gasolina: Double = 0.0
+    private var alcool: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        btCalcular()
+
+    }
+
+    private fun btCalcular() {
+        binding.buttonCalcular.setOnClickListener {
+            showResult()
+        }
+    }
+
+    private fun showResult() {
+
+        val alcool = binding.inputAlcool.text.toString().toDouble()
+        val gasolina = binding.inputGasolina.text.toString().toDouble()
+
+        val resultado: Double = alcool / gasolina
+
+
+        if (resultado >= 0.7){
+            binding.resultado.text = String.format("GASOLINA")
+        }
+        else{
+            binding.resultado.text = String.format("ÁLCOOL")
+            binding.bgResult.setBackgroundResource(R.drawable.background_gasolina)
+        }
 
     }
 }
